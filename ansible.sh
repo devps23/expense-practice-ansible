3886a00480c9859be07f8c2b7ddf95b9d31e3993
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-30 21:26:02 +0530

    vault

--- ansible.sh
+++ ansible.sh
@@ -2,7 +2,7 @@ component=$1
 env=$2
 vault_token=$3
 rm -f ~/*.json
-ansible-playbook get-secrets.yml vault_token=vault_token
+ansible-playbook get-secrets.yml -e vault_token=vault_token
 #ansible-playbook -i 172.31.84.158, expense.yml -e component_name=$component -e env=$env -e '@secrets.json'
 
 
