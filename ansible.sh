339ff727b9c80f05fb80c676133479a9bfd2fb0b
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-30 22:14:09 +0530

    ~/ indicates home directory

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,7 @@
 rm -f ~/*.json
 vault_token=$1
 ansible-playbook get-secrets.yml -e vault_token=$vault_token
-ansible-playbook -i 172.31.84.158, expense.yml -e component_name=$component -e env=$env -e '@secrets.json'
+ansible-playbook -i 172.31.84.158, expense.yml -e component_name=$component -e env=$env -e '@~/secrets.json'
 
 
 
