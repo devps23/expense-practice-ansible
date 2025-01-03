91e1ab68a8356388b407d85af0592ec1c209792e
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-03 21:27:20 +0530

    after creating json delete the files

--- ansible.sh
+++ ansible.sh
@@ -4,6 +4,7 @@ component=$2
 env=$3
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e component_name=$component -e env=$env
 ansible-playbook -i 172.31.84.158, expense.yml -e component_name=$component -e env=$env -e '@~/secrets.json' -e '@~/app.json'
+rm -f ~/*.json
 
 
 
