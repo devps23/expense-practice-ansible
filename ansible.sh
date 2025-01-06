ae223b64d81f3b0f1f631e76c777159c307be91d
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-06 13:23:40 +0530

    host name changed

--- ansible.sh
+++ ansible.sh
@@ -3,7 +3,7 @@ vault_token=$1
 component=$2
 env=$3
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e component_name=$component -e env=$env
-ansible-playbook -i ${env}-${component_name}.pdevops72.online, expense.yml -e component_name=$component -e env=$env -e '@~/secrets.json' -e '@~/app.json'
+ansible-playbook -i ${env}-${component}.pdevops72.online, expense.yml -e component_name=$component -e env=$env -e '@~/secrets.json' -e '@~/app.json'
 rm -f ~/*.json
 
 
